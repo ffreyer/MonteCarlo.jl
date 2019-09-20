4a7eaa8297868a6e6a7d59c2d122eef20bb7c3a6
Frederic Freyer <ffreyer@l45.thp.uni-koeln.de>
2019-09-20 13:32:38 +0200

    remnove old observables()

--- src/flavors/abstract.jl
+++ src/flavors/abstract.jl
@@ -20,32 +20,6 @@ Run the Monte Carlo Simulation.
 run!(mc::MonteCarloFlavor) = error("MonteCarloFlavor $(typeof(mc)) doesn't implement `run!`!")
 
 
-
-
-
-
-# general functions
-"""
-	observables(mc::MonteCarloFlavor)
-
-Get a list of all observables defined for a given Monte Carlo simulation.
-
-Returns a `Dict{String, String}` where values are the observables names and
-keys are short versions of those names. The keys can be used to
-collect correponding observable objects from the Monte Carlo simulation, e.g. like `mc.obs[key]`.
-
-Note, there is no need to implement this function for a custom `MonteCarloFlavor`.
-"""
-function observables(mc::MonteCarloFlavor)
-	obs = Dict{String, String}()
-	obsobjects = prepare_observables(mc, mc.model)
-	for (s, o) in obsobjects
-		obs[s] = MonteCarloObservable.name(o)
-	end
-	return obs
-end
-
-
 """
     reset!(mc::MonteCarloFlavor)
 
