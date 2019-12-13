53d6152a769838ae580552a7145d24b7dd528752
Frederic Freyer <ffreyer@l95.thp.uni-koeln.de>
2019-12-13 13:10:25 +0100

    optimize interaction_matrix_exp!

--- src/models/ZCTriangular/ZCmodel.jl
+++ src/models/ZCTriangular/ZCmodel.jl
@@ -138,14 +138,15 @@ This is a performance critical method.
 """
 @inline function interaction_matrix_exp!(mc::DQMC, m::ZCModel,
             result::Matrix, conf::ZCConf, slice::Int, power::Float64=1.)
-    dtau = mc.p.delta_tau
     # TODO optimize
     # compute this only once
+    dtau = mc.p.delta_tau
     lambda = acosh(exp(0.5m.U * dtau))
-    result .= Diagonal([
-        (exp.(sign(power) * lambda * conf[:,slice]))...,
-        (exp.(-sign(power) * lambda * conf[:,slice]))...
-    ])
+
+    result .= Diagonal(vcat(
+        exp.(sign(power) * lambda * conf[:, slice]),
+        exp.(-sign(power) * lambda * conf[:, slice])
+    ))
     nothing
 end
 
